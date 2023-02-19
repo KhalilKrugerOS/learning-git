9ae1a3e18c46d23bd60c93a9e892edb3a1905719
khalil lakhoua <86251644+KhalilKrugerOS@users.noreply.github.com>
2023-02-19 21:24:47 +0100

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".lorem-form");
+const result = document.querySelector(".result");
+var amount = document.getElementById("amount");
+
+form.addEventListener("submit", function showParagraph{
+// add code;
+});
